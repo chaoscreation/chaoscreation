eef3dec2b71191cd9942c7008bb10cbcee452206
Alok Menghrajani <alok@squareup.com>
2021-01-19 11:00:44 -0800

    lots of stuff

--- article.js
+++ article.js
@@ -3,7 +3,6 @@ $(document).ready(() => {
   rollDice()
   genTableOfContents()
   genWhiteNoise()
-//  genSquareLogo()
   genLCG()
   updateContributors()
   genSpoiler()
@@ -21,23 +20,40 @@ function cryptoRandomValue(min, max) {
   if ((range < 1) || (range > 256)) {
     throw new Error('invalid range')
   }
-  ok = false;
-  let bytes = new Uint8Array(1)
   let last = 0xff - (0xff % range);
+
+  let bytes = new Uint8Array(1)
+  crypto.getRandomValues(bytes);
 
   // Powers of two aren't biased.
   if (last + range == 0x100) {
     return (bytes[0] % range) + min;
   }
 
-  while (!ok) {
+  while (bytes[0] >= last) {
     crypto.getRandomValues(bytes);
-    if (bytes[0] < last) {
-      break;
-    }
   }
   return (bytes[0] % range) + min;
 }
+
+class FastCoinFlip {
+  constructor(bufSize) {
+    this.buf = new Uint8Array(bufSize)
+    this.ptr = 0;
+  }
+
+  flip() {
+    if (this.ptr == 0) {
+      crypto.getRandomValues(this.buf);
+    }
+    let byte = this.buf[(this.ptr/8)|0];
+    let r = (byte >> (this.ptr%8))&1;
+    this.ptr = (this.ptr + 1) % (this.buf.length * 8);
+
+    return r;
+  }
+}
+
 
 function font() {
   const n = (Math.random() * 3)|0;
@@ -55,20 +71,25 @@ function genTableOfContents() {
   })
 }
 
+function scaleCanvas(el, ctx, width, height) {
+  const devicePixelRatio = window.devicePixelRatio || 1;
+  ctx.width = width * devicePixelRatio;
+  ctx.height = height * devicePixelRatio;
+  ctx.scale(devicePixelRatio, devicePixelRatio);
+  el.width = ctx.width;
+  el.height = ctx.height;
+}
+
 function genWhiteNoise() {
-  let x = 200;
-  let y = 200;
   let ctx = fig1.getContext('2d');
-  fig1.width = x;
-  fig1.height = y;
+  scaleCanvas(fig1, ctx, 200, 200)
 
-  ctx.clearRect(0, 0, x, y);
-  let bytes = new Uint8Array(x * y);
-  crypto.getRandomValues(bytes);
+  ctx.clearRect(0, 0, ctx.width, ctx.height);
+  let rng = new FastCoinFlip(1000);
 
-  for (let i=0; i<y; i++) {
-    for (let j=0; j<x; j++) {
-      if (bytes[i*x+j]<128) {
+  for (let i=0; i<ctx.height; i++) {
+    for (let j=0; j<ctx.width; j++) {
+      if (rng.flip() == 0) {
         ctx.fillRect(i, j, 1, 1);
       }
     }
@@ -76,11 +97,8 @@ function genWhiteNoise() {
 }
 
 function genLCG() {
-  let x = 200;
-  let y = 200;
   let ctx = fig2.getContext('2d');
-  fig2.width = x;
-  fig2.height = y;
+  scaleCanvas(fig2, ctx, 200, 200)
 
   // LCG with deliberately poor parameters. These parameters were used by
   // Visual Basic 6 and earlier. Some seeds, such as 15461763 or 343 are really
@@ -88,98 +106,38 @@ function genLCG() {
   let M = 16777216;
   let A = 1140671485;
   let C = 12820163;
-  let seed = ((Math.random() * M)|0);
+  let seed = (Math.random()*M)|0;
 
-  ctx.clearRect(0, 0, x, y);
-  let bytes = new Uint8Array(x * y);
-  for (let i=0; i<x*y; i++) {
-    bytes[i] = seed % 256;
-    seed = (A * seed + C) % M;
-  }
+  ctx.clearRect(0, 0, ctx.width, ctx.height);
 
-  for (let i=0; i<y; i++) {
-    for (let j=0; j<x; j++) {
-      if (bytes[i*x+j]<128) {
+  for (let i=0; i<ctx.height; i++) {
+    for (let j=0; j<ctx.width; j++) {
+      let r = seed % 2;
+      seed = (A * seed + C) % M;
+      if (r == 0) {
         ctx.fillRect(i, j, 1, 1);
       }
     }
   }
 }
-
-
-function genSquareLogo() {
-  let x = 200;
-  let y = 200;
-  let ctx = fig3.getContext('2d');
-  fig3.width = x;
-  fig3.height = y;
-
-  ctx.clearRect(0, 0, x, y);
-  let bytes = new Uint8Array(x * y);
-  crypto.getRandomValues(bytes);
-
-  for (let i=0; i<y; i++) {
-    for (let j=0; j<x; j++) {
-      let cutoff = SqLogo.in(i, j) ? 192 : 64;
-      if (bytes[i*x+j]<cutoff) {
-        ctx.fillRect(i, j, 1, 1);
-      }
-    }
-  }
-}
-
-class SqLogo {
-  static in(x, y) {
-    x = Math.abs(100-x);
-    y = Math.abs(100-y);
-    return SqLogo.inner(x, y) || SqLogo.outer(x, y);
-  }
-
-  static inner(x, y) {
-    return SqLogo.in_rounded_square(x, y, 10.6, 3);
-  }
-
-  static outer(x, y) {
-    return !SqLogo.in_rounded_square(x, y, 26.6, 5.2) &&
-      SqLogo.in_rounded_square(x, y, 33.2, 16.8);
-  }
-
-  static in_rounded_square(x, y, dist, radius) {
-    return SqLogo.in_circle(x, y, dist, dist, radius) ||
-      SqLogo.in_rect(x, y, dist, dist + radius) ||
-      SqLogo.in_rect(x, y, dist + radius, dist);
-  }
-
-  static in_circle(x, y, cx, cy, r) {
-    return ((x-cx)*(x-cx) + (y-cy)*(y-cy)) < r*r;
-  }
-
-  static in_rect(x, y, width, height) {
-    return (x < width) && (y < height);
-  }
-}
-
 
 function updateContributors() {
-  // shuffle contributors using Fisher-Yates shuffle
-  let contributorsList = ["Oscar", "Alex", "Michael"];
-  contributorsList = shuffle(contributorsList);
-
+  shuffledList = shuffle($('#contributors').children());
   $('#contributors').empty();
-  for (let i=0; i<contributorsList.length; i++) {
-    $('#contributors').append($('<li>').text(contributorsList[i]));
-  }
+  $('#contributors').append(shuffledList);
 }
 
+// Fisher-Yates shuffle (aka Knuth shuffle)
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = cryptoRandomValue(0, i+1);
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
 function genSpoiler() {
